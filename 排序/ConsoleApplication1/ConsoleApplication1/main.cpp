2fc6fcbb006d19474b19f48a19d6218c80d8a9b6
youjunmao <942357585@qq.com>
2018-01-18 11:33:04 +0800

    m

--- 排序/ConsoleApplication1/ConsoleApplication1/main.cpp
+++ 排序/ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void	swap(int*	_array, int _left, int _right)
+{
+	int	_temp = _array[_left];
+	_array[_left] = _array[_right];
+	_array[_right] = _temp;
+}
+
 void	printArray(int*	_array, int _len)
 {
 	for (int i = 0; i < _len; i++)
@@ -85,13 +92,109 @@ void	sortShell(int	*_array, int _len)
 	} while (gap > 1);
 }
 
- int  main()
+//划分过程，第一个元素当枢轴，分成2个有效子序列
+int		partition(int _array[], int low, int high)
+{
+	int	_pv = _array[low];
+	
+	while (low<high)
+	{
+		while ((low<high)&&(_array[high]>=_pv))
+		{
+			high--;//比基准大，本来就在右边，所以high前移动
+		}
+		swap(_array,low,high);
+		while ((low<high)&&(_array[low]<=_pv))
+		{
+			low++;
+		}
+		swap(_array, low, high);
+	}
+	return	low;//返回枢轴的位置
+}
+
+void	QSort(int	_array[], int low, int high)
+{
+	if (low<high)
+	{
+		//选一个pv值，把数据分别放在pv值的左右两边，并把pivot位置返回出来
+		int	pivot = partition(_array, low, high);
+		//对子序列1排序
+		QSort(_array, low, pivot - 1);
+		//对子序列2排序
+		QSort(_array, pivot + 1, high);
+	}
+}
+//快速排序：让数据分成两部分；partition  n(logn)
+void	QuickSort(int	_array[], int len)
+{
+	QSort(_array, 0, len - 1);
+}
+
+void	Merge(int src[], int des[], int low, int mid, int high)
+{
+	int i = low;
+	int j = mid + 1;
+	int k = low;
+
+	while ((i<=mid)&&(j<=high))//将小的放到目的地中
+	{
+		if (src[i]<src[j])
+		{
+			des[k++] = src[i++];
+		}
+		else
+		{
+			des[k++] = src[j++];
+		}
+	}
+
+	while (i<=mid)//若还剩几个尾部元素
+	{
+		des[k++] = src[i++];
+	}
+
+	while (j<=high)
+	{
+		des[k++] = src[j++];
+	}
+}
+//每次分为两路，当只剩下一个元素时，就不需要再划分
+void	MSort(int	src[], int	des[], int low, int high, int max)
+{
+	if (low==high)//只有一个元素，不需要归并，结果赋给des[low]
+	{
+		des[low] = src[low];
+	}
+	else
+	{
+		int		mid = (low + high) / 2;
+		int*	space = (int*)malloc(sizeof(int)*max);
+
+		//递归进行两路，两路的划分
+		//当剩下一个元素时，递归划分结束，然后开始merge归并操作
+		if (space!=NULL)
+		{
+			MSort(src, space, low, mid, max);
+			MSort(src, space, mid + 1, high, max);
+			Merge(space, des, low, mid, high);//调用归并函数进行归并
+		}
+
+		free(space);
+	}
+}
+//归并排序：牺牲空间换取时间 nlogn
+void	MergeSort(int	_array[], int len)
+{
+	MSort(_array, _array, 0, len - 1, len);
+}
+int  main01()
 {
 
 	int	_array[] = {3,56,9,0,5,5,3};
 	int	_len = sizeof(_array) / sizeof(*_array);
 	printArray(_array, _len);
-	sortShell(_array, _len);
+	MergeSort(_array, _len);
 	printArray(_array, _len);
 
 	cout<<"排序"<<endl;
